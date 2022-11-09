9b7d729470dc70f7e383af71ba11e07e8ab5f414
ericescobar2020 <ericescobar2020@outlook.com>
2022-11-09 12:18:57 -0300

    clase 2 finalizado

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const path = require("path")
+// const path = require("path")
 
 // requerimos dotenv para acceder a nuestro .env
 require("dotenv").config();
@@ -9,13 +9,27 @@ const express = require('express');
 // Inicializamos nuestra aplicacion express
 const app = express();
 
+// Configurar nuestro app 
+app.use(express.static("src/views"));
 
+// Requerimos los enrutadores 
+const homeRoutes = require("./routes/home");
+const productsRoutes = require("./routes/products");
+// Utilizar nuestro enrutadores
+app.use("/", homeRoutes);
+app.use("/products", productsRoutes);
 
-
-// //Creamos nuestro primer GET
-app.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname,"views/home.html"));
-});
+// // //Creamos nuestro primer GET (ruta) // nuestro primer ruta
+// app.get("/", (req, res) => {
+//     // json 
+//     //res.json({key: value})
+//     //plain text
+//     //res.send ("asd")
+//     //HTTP status
+//     //res.status(200).send()
+//     // files
+//     res.sendFile(path.join(__dirname,"views/home.html"));
+// });
 
 //Comenzamos a escuchar con nuestro servidor 
 app.listen(process.env.PORT,() => {
---
+++ src/routes/home/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const express = require("express");
+
+const router = express.Router();
+
+// Nuestra primer ruta 
+router.get("/", (req, res) => {
+  // file 
+    res.status(200).sendFile(path.join(__dirname,"../../views/html/home.html"));
+});
+
+module.exports= router; 
